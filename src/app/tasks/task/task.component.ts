7519d6fa654c20394b0da011b6ec79af87a89edd
Ujjwal Kumar Agrawal <ujjwalagrawal29102001@gmail.com>
2024-11-30 17:13:30 +0530

    Added Date Pipe

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,9 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { type Task } from './task.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-task',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
